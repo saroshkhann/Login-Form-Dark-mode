5b122585f218698842861019b78da60a1a8d9176
saroshkhann <ksarosh785@gmail.com>
2024-08-16 19:04:40 +0500

    main

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const inputEmail = document.querySelector('.--1');
+const inputPassword = document.querySelector('.--2');
+const input3 = document.querySelector('.--3');
+const btnLogin = document.querySelector('.form__data--btn');
+const btnSubmit = document.querySelector('.submit');
+const btnForget = document.querySelector('.form__data--forget');
+const container = document.querySelector('.contain');
+
+const account = {
+  email: 'ksarosh785@gmail.com',
+  password: '123',
+};
+
+btnLogin.addEventListener('click', e => {
+  e.preventDefault();
+  if (
+    account.email === inputEmail.value &&
+    account.password === inputPassword.value
+  ) {
+    alert('correct');
+  } else {
+    alert('wrong');
+  }
+});
